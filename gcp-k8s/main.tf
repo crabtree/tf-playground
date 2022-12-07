968b8e1fd7212b3f1d2e8841a81e1ed590cbcd99
crabtree <3641496+crabtree@users.noreply.github.com>
2022-12-07 23:38:52 +0100

    Update main.tf

--- gcp-k8s/main.tf
+++ gcp-k8s/main.tf
@@ -1,3 +1,7 @@
+provider "google" {
+  zone        = var.zone
+}
+
 resource "google_compute_network" "vpc_network" {
   name                    = var.vpc_network_name
   auto_create_subnetworks = true
